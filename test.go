a6b12fa27572199e975e4fa2fe39d3a007d10e53
MacBook Pro <macbookpro@Muhammaddiyor.local>
2022-02-23 11:15:31 +0500

    test.go modified by Muhammaddiyor

--- test.go
+++ test.go
@@ -8,15 +8,4 @@ func car() {
 
 func main() {
 
-	num := [5]int{3, 5, 7, 8, 9}
-	var check int
-	for i, v := range num {
-		check = num[i]
-		for j, k := range num {
-			if check == k {
-				delete(num, j)
-			}
-		}
-	}
-
 }
